def9edffc900302c0c78f5f2ecf00e9b117d3cef
Cameron Schwartz <cam.schwartz@gmail.com>
2015-11-19 00:47:32 -0800

    Initial commit
    Includes initial Photon firmware (helloworld.ino) and PowerShell script
    to call cloud function (ActivateLock.ps1).

---
+++ ActivateLock.ps1
@@ -0,0 +1,18 @@
+﻿
+[string] $ParticleAPIBaseUri = "https://api.particle.io/v1/devices/"
+
+# This section defines connection properties for the Photon device to be used
+[string] $ParticleDeviceID = "30002d000547343233323032"
+[string] $ParticleAccessToken = "9d9ad498497bda7e6fb90d96af6375fb08d0cad4"
+
+# Function to be called
+[string] $ParticleAppName = "activatelock"
+
+# Build the full URI
+$postUri = $ParticleAPIBaseUri + $ParticleDeviceID + "/" + $ParticleAppName
+
+# Build the POST payload
+$postParams = @{access_token=$ParticleAccessToken;}
+
+# Make the call
+$WebResponse = Invoke-WebRequest -Uri $postUri -Method Post -Body $postParams
